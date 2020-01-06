3245ac33d54c53ab9a45b9e1ed0b969761b59217
gkgoat <gkgoat6700@gmail.com>
2020-01-06 11:03:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,51 @@ except Exception: pass
 eb = []
 uib = []
 mem = map([{'type': 'null'}] * 20000,lambda i: i.copy())
+class BrowserDisplay:
+    QUIT = 'quit'
+    def __init__(self,the_element):
+        self._events = []
+        def on_event(evt):
+            self._events.append(evt)
+        the_element.bind('mousedown',on_event)
+        the_element.bind('mouseup',on_event)
+        the_element.bind('keydown',on_event)
+        the_element.bind('keyup',on_event)
+    def get_events(self):
+        e = self._events
+        self._events = []
+        return e
 async def p_eval(item,vector):
     pass
 def pg_main():
     global uib, eb
-    pygame.init()
-    display = pygame.display.set_mode((600,600))
+    try:
+        pygame.init()
+        display = pygame.display.set_mode((600,600))
+        display.get_events = pygame.event.get
+        display.QUIT = pygame.QUIT
+    except Exception:
+        display = BrowserDisplay(None)
     done = False
     while not done:
         for ui in uib:
+            yield {'type': 'uiexec','data': ui}
             ui(display)
         uib = []
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: 
+        for event in display.get_events():
+            if event.type == display.QUIT: 
                 done = True
                 break
             else:
+                yield {'type': 'handled_event','data': event}
                 eb.append(event)
 def vm_main():
     vector = []
     async def boot():
-        for i in vector:
+        nonlocal vector
+        while len(vector) != 0:
+            i = vector[-1]
+            vector = vector[:-1]
             try:
                 await i.eval(vector)
             except Exception:
